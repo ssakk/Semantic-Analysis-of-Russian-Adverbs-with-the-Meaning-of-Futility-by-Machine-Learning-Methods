5141202d756ebb7fe2e72168cca64b20192fba97
ssakk <agzenina@edu.hse.ru>
2024-05-24 23:13:48 +0300

    add data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Semantic-Analysis-of-Russian-Adverbs-with-the-Meaning-of-Futility-by-Machine-Learning-Methods
-All materials for Bachelor Thesis "Semantic Analysis of Russian Adverbs with the Meaning of Futility by Machine Learning Methods"
+# Semantic Analysis of Russian Adverbs with the Meaning of Futility by Machine Learning Methods
+Here are all materials for Bachelor Thesis "Semantic Analysis of Russian Adverbs with the Meaning of Futility by Machine Learning Methods"
+
+- scripts
+- subcorpus
+- word classes
--- code/word_classes/classification.py
+++ code/word_classes/classification.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import csv
+import json
 
 import gensim
 import numpy as np
@@ -43,32 +44,47 @@ class Clustering:
         scaler = MinMaxScaler()
         return scaler.fit_transform(vectors)
 
-    def best_cluster_number(self, word: str, viz: bool = True) -> Tuple[int, List[int]]:
+    def best_cluster_number(self, name: str, viz: bool = True) -> Tuple[int, List[int]]:
         silhouette_scores = []
-        for i in range(10, 101):
+        for i in range(10, 201):
             print(i)
             preds = self.perform_cluster(n_clusters=i)
             silhouette = silhouette_score(self.vectors, preds)
             silhouette_scores.append(silhouette)
         if viz:
             plt.figure(figsize=(10, 4))
-            plt.scatter(x=[i for i in range(10, 101)], y=silhouette_scores, s=70, c='#a689c9')
+            plt.scatter(x=[i for i in range(10, 201)], y=silhouette_scores, s=50, c='#a689c9')
             plt.grid(True)
             plt.xlabel("Количество кластеров", fontsize=15)
             plt.ylabel("Коэффициент силуэта", fontsize=15)
-            plt.xticks(range(10, 101, 10), fontsize=15)
+            plt.xticks(range(10, 201, 10), fontsize=15)
             plt.yticks(fontsize=15)
-            plt.savefig(f'{word}.png')
+            plt.savefig(f'{name}.png')
             plt.show()
         return silhouette_scores.index(max(silhouette_scores)) + 10, silhouette_scores
 
-    def perform_cluster(self, n_clusters: int) -> np.ndarray:
+    def perform_cluster(self, n_clusters: int, final: bool = False, name: str = None) -> np.ndarray:
         clst = AgglomerativeClustering(n_clusters=n_clusters)
         preds = clst.fit_predict(self.vectors)
+
+        if final:
+            res = {}
+            for word, word_class in zip(self.words, preds):
+                res[word] = str(word_class)
+            with open(f'{name}.json', 'w', encoding='utf-8') as f:
+                json.dump(res, f, ensure_ascii=False)
+
         return preds
 
 
 path_subjects = '../subcorpus/all_subjects.csv'
 path_verbs = '../subcorpus/all_verbs.csv'
-xu, z = Clustering(path_subjects, norm=True).best_cluster_number('subj_with_norm')
-print(xu, z)
+
+S = Clustering(path_subjects)
+V = Clustering(path_verbs)
+
+# num_subject_clusters = S.best_cluster_number('subj_num_clusters')
+# num_verb_clusters = V.best_cluster_number('verb_withot_norm')
+# print(num_subject_clusters, num_verb_clusters)
+pred_subjects = S.perform_cluster(35, final=True, name='subjects_classes')
+pred_verbs = V.perform_cluster(200, final=True, name='verbs_classes')
